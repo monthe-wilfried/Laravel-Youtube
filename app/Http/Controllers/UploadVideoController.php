9639b830bb728b1fe4b1a27e216584c3a0dd2b35
Monthe Wilfried <monthetcham@yahoo.fr>
2020-06-25 22:22:57 +0200

    Created a streamable job queue and videos converted in different streamable format

--- app/Http/Controllers/UploadVideoController.php
+++ app/Http/Controllers/UploadVideoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Channel;
+use App\Jobs\Videos\ConvertForStreaming;
 use Illuminate\Http\Request;
 
 class UploadVideoController extends Controller
@@ -13,9 +14,13 @@ class UploadVideoController extends Controller
     }
 
     public function store(Channel $channel){
-        $channel->videos()->create([
+        $video = $channel->videos()->create([
             'title' => request()->title,
             'path' => request()->video->store('channels/'.$channel->id)
         ]);
+
+        $this->dispatch(new ConvertForStreaming($video));
+
+        return $video;
     }
 }
